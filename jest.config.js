cba1649ee858ad4d7c17671dfe3cee884706aaf6
Victor Garcia <victorgarciaparis13@gmail.com>
2021-07-21 17:27:19 +0200

    Removed jest

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  moduleFileExtensions: ['js', 'ts'],
-  testRegex: '(/tests/.*|(\\.|/)(test|spec))\\.(jsx?|tsx?)$',
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-  },
-  testURL: 'http://localhost/',
-  testEnvironment: 'jsdom',
-};
